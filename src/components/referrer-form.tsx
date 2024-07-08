72dc0e5198819bc7a0465e68c6b8f8c203e21a54
TusharRathi108 <tushar.rathi.tech@gmail.com>
2024-07-08 10:46:17 +0530

    added render url to env and sweetalert2 for submission

--- src/components/referrer-form.tsx
+++ src/components/referrer-form.tsx
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import axios from "axios";
-import { useEffect } from "react";
+import Swal from "sweetalert2";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ReferralFormSchema } from "@/schemas/referal-form";
@@ -23,6 +24,9 @@ interface ReferrerFormProps {
   onClose: () => void;
 }
 
+// Render Node.js URL
+const renderNodeUrl = import.meta.env.VITE_RENDER_URL;
+
 const ReferrerForm = ({ onClose }: ReferrerFormProps) => {
   // use react-hook-form for getting form values
   const { control, handleSubmit, formState } = useForm<FormSchema>({
@@ -31,12 +35,15 @@ const ReferrerForm = ({ onClose }: ReferrerFormProps) => {
       rname: "",
       remail: "",
       rphone: "",
-      name: "",
-      email: "",
-      phone: "",
       message: "",
+      email: "",
+      name: "",
+      phone: "",
     },
   });
+
+  // use-state to disable submit button while sending mail and saving data
+  const [isloading, setIsLoading] = useState(false);
 
   // Form-errors from zod
   const formError = () => {
@@ -58,21 +65,58 @@ const ReferrerForm = ({ onClose }: ReferrerFormProps) => {
   useEffect(formError, [formState.errors]);
 
   // submit form data
-  const onSubmit: SubmitHandler<FormSchema> = async (formData) => {
+  const onSubmit: SubmitHandler<FormSchema> = async (formData: any) => {
     try {
-      // post form data to DB
-      const response = await axios.post(
-        "http://localhost:3000/api/refer",
-        formData
-      );
+      // // set isLoading to true
+      setIsLoading(true);
 
-      toast.success(response.data.success);
+      // Show loading spinner
+      Swal.fire({
+        title: "Submitting...",
+        text: "Please wait while we submit your referral!",
+        icon: "question",
+        background: "rgb(15 23 42)",
+        color: "white",
+        allowOutsideClick: false,
+        showConfirmButton: false,
+        willOpen: () => {
+          Swal.showLoading();
+        },
+      });
+
+      // post form data to DB
+      const response = await axios.post(`${renderNodeUrl}/api/refer`, formData);
+
+      // Update SweetAlert2 modal with success message
+      Swal.fire({
+        icon: "success",
+        title: "Referral submitted!",
+        text: response.data.success,
+        background: "rgb(15 23 42)",
+        color: "white",
+        showConfirmButton: true,
+      });
+
+      // close dialog after submission
       onClose();
     } catch (error) {
       if (axios.isAxiosError(error)) {
-        console.log(error);
-        // toast.error(error.response?.data);
+        // show toast message on error
+        Swal.fire({
+          icon: "error",
+          title: "Referral submisson failed!",
+          text: error.response?.data.error,
+          background: "rgb(15 23 42)",
+          color: "white",
+          showConfirmButton: true,
+        });
+
+        // set isLoading to false
+        setIsLoading(false);
       }
+    } finally {
+      // set isLoading to true
+      setIsLoading(false);
     }
   };
 
@@ -137,13 +181,11 @@ const ReferrerForm = ({ onClose }: ReferrerFormProps) => {
               />
             )}
           />
-
           {/* Divider - Two */}
           <div className="border-dashed border border-slate-200 mt-3" />
           <div className="flex justify-center">
             <IoDiamondSharp className="absolute top-[316px] text-3xl" />
           </div>
-
           {/* Form - One */}
           <Label className="text-center"> Your Details </Label>
           {/* Name */}
@@ -162,7 +204,6 @@ const ReferrerForm = ({ onClose }: ReferrerFormProps) => {
               />
             )}
           />
-
           {/* Email */}
           <Label htmlFor="terms">Email</Label>
           <Controller
@@ -179,7 +220,6 @@ const ReferrerForm = ({ onClose }: ReferrerFormProps) => {
               />
             )}
           />
-
           {/* Phone Number */}
           <Label htmlFor="terms">Phone Number</Label>
           <Controller
@@ -196,7 +236,6 @@ const ReferrerForm = ({ onClose }: ReferrerFormProps) => {
               />
             )}
           />
-
           {/* Tell us more about your referral */}
           <Label htmlFor="terms">Tell us about yourself!</Label>
           <Controller
@@ -212,11 +251,11 @@ const ReferrerForm = ({ onClose }: ReferrerFormProps) => {
               />
             )}
           />
-
           <Button
             type="submit"
             variant={"outline"}
             className="bg-slate-900 text-slate-100 shadow-inner shadow-slate-100 uppercase border hover:bg-slate-100 hover:text-slate-900 hover:shadow-slate-900 hover:border-slate-950 transition duration-200 ease-linear"
+            disabled={isloading}
           >
             {" "}
             Refer{" "}
